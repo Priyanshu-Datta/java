e92466a38d6e487bc3644b8f7a2549956d15eb97
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-07-21 23:55:39 +0530

    first thread code

---
+++ thread1.java
@@ -0,0 +1,32 @@
+class runthread implements Runnable{
+	
+	public void run(){
+		try{
+			for( int i=10;i>=1;i--){
+			System.out.println(i);
+			Thread.sleep(500);}
+		}catch(InterruptedException e){
+			System.out.println(e);
+		}
+	
+	}
+}
+
+class thread1{
+	public static void main(String[] args){	
+	
+	Thread t1 = new Thread(new runthread());
+	t1.start();
+	
+	try{
+		for(int i=10;i>=1;i--){
+		System.out.println(i);
+		Thread.sleep(500);}
+		}catch(InterruptedException e){
+		System.out.println(e);
+	}
+}
+}
+
+
+	
