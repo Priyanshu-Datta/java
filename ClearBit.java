4cdeed7516c6c292ae21b43eb66f58cf5b226c5d
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-07-28 21:20:03 +0530

    Clear Bit function

---
+++ ClearBit.java
@@ -0,0 +1,20 @@
+package bitmanipulation;
+
+import java.util.Scanner;
+
+public class ClearBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number:");
+        int n = sc.nextInt();
+        System.out.println("enter the position you make Zero:");
+        int pos =sc.nextInt();
+
+        int bitMask = 1<<pos;
+
+        int n2 = ~(bitMask) & n;
+        System.out.println("the new number:" + n2);
+        sc.close();
+     }
+}
