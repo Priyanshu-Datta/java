6704de79a6e908ca66153c782d089d2199fa81c0
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-07-28 21:11:15 +0530

    SetBit function

---
+++ getbit.java
@@ -0,0 +1,23 @@
+package java;
+
+import java.util.Scanner;
+
+public class getbit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number:");
+        int n = sc.nextInt();
+        System.out.println("enter the position you want to get:");
+        int pos =sc.nextInt();
+
+        int bitMask = 1<<pos;
+
+        if((bitMask & n) == 0){
+            System.out.println("the Bit is Zero.");
+        }else{
+            System.out.println("the Bit is One.");
+        }
+        sc.close();
+    }
+}
