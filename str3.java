5022ef31d7d2133a6146405de6a76a552b6c1bea
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-08-05 23:57:21 +0530

    somr methods in string

---
+++ str3.java
@@ -0,0 +1,14 @@
+
+
+public class str3 {
+    public static void main(String[] args) {
+        String s1="Rupam";
+        String s2="rupam";
+
+        if(s1.compareTo(s2)==0){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+    }
+}
---
+++ str4.java
@@ -0,0 +1,13 @@
+
+
+public class str4 {
+    public static void main(String[] args) {
+    StringBuilder sb1= new StringBuilder("priyanshu");
+    String s=" datta";
+    sb1.insert(sb1.length(),s);
+    System.out.println(sb1);
+
+    }
+
+
+}
